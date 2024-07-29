b50b1c2c2002a86dc4342051ca9b01b946ec93f3
Albert Ge <lbertge@gmail.com>
2024-07-29 15:40:40 -0500

    Change data directory for script

--- scripts/ni/ft/spanish_qg/skillit.sh
+++ scripts/ni/ft/spanish_qg/skillit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-for SEED in 0 1 2 3 4
+for SEED in 0
 do 
     python3 main.py \
         --task_name ni \
-        --train_data_dir /dfs/scratch0/mfchen/natural-instructions \
-	    --val_data_dir /dfs/scratch0/mfchen/natural-instructions \
+        --train_data_dir ~/natural-instructions \
+	    --val_data_dir ~/natural-instructions \
         --dev_split_path ./aux_data/xlingual_dev_split_map.pkl \
         --ni_task_info_path ./aux_data/ni_xlingual_task_info.pkl \
         --selection_seed ${SEED} \
-        --max_steps 600 \
+        --max_steps 100 \
         --xlingual \
         --slice_list question_answering english english question_answering spanish spanish question_generation english english question_generation spanish spanish \
         --k 4 \
@@ -21,5 +21,5 @@ do
         --update_steps 100 \
         --graph_path ./ni_graphs/spanish_qg_normalized.npy \
         --filter_val_skills \
-        --num_ckpts 6
+        --num_ckpts 1
 done 
